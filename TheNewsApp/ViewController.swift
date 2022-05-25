7c0e185412245d8a51a4ddb6bf6c707acdfbc251
Frederico Lacis <fredlacispro@live.com>
2022-05-24 22:23:24 -0300

    Initial Commit

---
+++ TheNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheNewsApp
+//
+//  Created by Frederico Lacis de Carvalho on 24/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
